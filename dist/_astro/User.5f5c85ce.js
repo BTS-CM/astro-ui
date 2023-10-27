2fb3578c2fdac28f5aeb5cab011a4fa6b1f10af5
CM <grctestemail@gmail.com>
2023-10-27 20:15:34 +0100

    Updated dependencies

--- dist/_astro/User.5f5c85ce.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./CurrentUser.c838994d.js";const[r]=o({fetcher:async(s,a)=>{const t=await fetch(`http://localhost:8080/api/getAccountBalances/${s}/${a}`,{method:"GET"});if(!t.ok){console.log("Failed to fetch user balances");return}const e=await t.json();if(e&&e.result)return console.log("Fetched user balances"),e.result}});export{r as c};
